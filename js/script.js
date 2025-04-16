40746905eb7a76248182f8f2293c8c383ffc9bcd
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-04-16 19:58:08 +0300

    add: updateImageSrc js function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,40 @@
-'use strict';
+"use strict";
 
-flatpickr('#lot-date', {
-  enableTime: false,
-  dateFormat: "Y-m-d",
-  locale: "ru"
+document.addEventListener("DOMContentLoaded", () => {
+  flatpickr("#lot-date", {
+    enableTime: false,
+    dateFormat: "Y-m-d",
+    locale: "ru",
+  });
+
+  // Display Input File Image
+  const updateImageSrc = () => {
+    const fileInputs = document.querySelectorAll('input[type="file"]');
+
+    fileInputs.forEach((fileInput) => {
+      fileInput.addEventListener("change", (evt) => {
+        // Check type
+        const file = evt.target?.files?.[0];
+
+        if (file && file.type.startsWith("image")) {
+          // Create URL
+          const imgUrl = URL.createObjectURL(file);
+
+          // Get image node
+          const image = fileInput.previousElementSibling;
+
+          if (image) {
+            // Set src
+            image.src = imgUrl;
+          }
+        } else {
+          fileInput.value = null;
+
+          alert("File is not image!");
+        }
+      });
+    });
+  };
+
+  updateImageSrc();
 });
