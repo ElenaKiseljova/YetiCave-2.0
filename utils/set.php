003204d66d2ee0f4cde1130ee153d3d6423d7399
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-04-22 22:15:59 +0300

    upd: set default timezone

--- utils/set.php
+++ utils/set.php
@@ -1,6 +1,10 @@
 <?php
 require_once 'controllers/DBController.php';
 
+define('TIMEZONE', 'Europe/Kyiv');
+
+date_default_timezone_set(TIMEZONE);
+
 // Connect to the database
 $db = new DBController();
 
